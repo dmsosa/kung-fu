b0ae988c145105b7991a5f353f4739dc51755272
Durian <dmsosa21@outlook.com>
2022-12-31 14:11:37 +0000

    Eine fantastisch Arbeit gemachen

--- html project/script/condensed.js
+++ html project/script/condensed.js
@@ -2,10 +2,25 @@
 
 // console.log("Hello ",username);
 
-let username; 
+// let a, b; 
 
-document.getElementById("myButton").onclick = function(){
-    username = document.getElementById("testing").value;
-    console.log(username);
-    document.getElementById("Titulo").innerHTML = "Hello "+username;
+// document.getElementById("myButton").onclick = function(){
+//     hyp = document.getElementById("testing").value;
+//     console.log(hyp);
+//     document.getElementById("Titulo").innerHTML = "THe hypotenuse is "+ hyp;
+// }
+
+let a = window.prompt("enter a side A");
+let b;
+let hyp;
+
+document.getElementById("MyButton").onclick = function() {
+    b = document.getElementById("testing").value;
+    document.getElementById("Titulo").innerHTML = "Wooo!!"
 }
+
+hyp = math.sqrt((Math.pow(a, 2) + Math.pow(b, 2)));
+
+console.log("The hypotenuse is. . . " hyp);
+let rounded = Math.abs(circumference);
+console.log(rounded);
